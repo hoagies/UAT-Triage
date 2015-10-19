d9459ebc7f91edd1938ac39514976814d0092ac6
hoagies <sphogan85@gmail.com>
2015-10-19 21:34:18 +0000

    updating project scope

--- App.js
+++ App.js
@@ -2,68 +2,98 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 	launch: function() {
-			
-		// -----------------------
-		// FILTER BY TESTING TYPE?
-		// -----------------------
 		
-		var columns = [
-			{
-				value: 'CAT 0: Triage',
-				columnHeaderConfig: {
-					headerData: {triageVerdict: 'CAT 0: Triage'}
-				}
-			},
-			{
-				value: 'CAT 1: OAF Defect',
-				columnHeaderConfig: {
-					headerData: {triageVerdict: 'CAT 1: OAF Defect'}
-				} 
-			},
-			{
-				value: 'CAT 2: OAF Requirement',
-				columnHeaderConfig: {
-					headerData: {triageVerdict: 'CAT 2: OAF Requirement'}
-				} 
-			},
-			{
-				value: 'CAT 3: OAF Training',
-				columnHeaderConfig: {
-					headerData: {triageVerdict: 'CAT 3: OAF Training'}
-				} 
-			},
-			{
-				value: 'CAT 4: Non-OAF',
-				columnHeaderConfig: {
-					headerData: {triageVerdict: 'CAT 4: Non-OAF'}
-				} 
-			}
-		];
+		// Radian
+		var project_oid = '/project/37192747640';
+		// AIT Sandbox
+		// var project_oid = '/project/28269494803';
+		// console.log('project_oid: ',project_oid);
 
 		this.add({
+			xtype: 'rallycombobox',
+			width: 600,
+			fieldLabel: 'Select Initiative:',
+			// Display Template
+			displayTpl: Ext.create('Ext.XTemplate','<tpl for=".">','{FormattedID} - {Name}','</tpl>'),
+			// List Template
+			tpl: Ext.create('Ext.XTemplate','<tpl for=".">','<div class="x-boundlist-item">{FormattedID} - {Name}</div>','</tpl>'),
+			storeConfig: {
+				autoLoad: true,
+				model: 'PortfolioItem/Initiative',
+				fetch: ['FormattedID', 'Name'],
+				sorters: [
+					{
+						property: 'ObjectID',
+						direction: 'ASC'
+					}
+				],
+				remoteGroup: false,
+				remoteSort: false,
+				remoteFilter: false,
+				limit: Infinity,
+				context: {
+					project: project_oid,
+					projectScopeDown: true,
+					projectScopeUp: false
+				}
+			},
+			// stateful: false,
+			listeners: {
+				select: this._onSelect,
+				ready: this._onLoad,
+				scope: this
+			}
+		});
+	},
+		
+	_onLoad: function() {
+		var project_oid = '/project/37192747640';
+		
+		this.add({
 			xtype: 'rallycardboard',
-			types: ['Defect'],
-			attribute: 'c_TriageVerdict',
-			context: this.getContext(),
-			readOnly: true,
-			columnConfig: {
-				columnHeaderConfig: {
-					headerTpl: '{triageVerdict} TEST'
+			types: ['PortfolioItem/Feature'],
+			attribute: 'State',
+			storeConfig: {
+				context: {
+					project: project_oid,
+					projectScopeDown: true,
+					projectScopeUp: false
 				},
+				filters: [this._getFilter()]
+			},
+			columnConfig: {
 				plugins: [
 					{ptype: 'rallycolumncardcounter'}
 				]
 			},
-			columns: columns,
 			cardConfig: {
 				showIconsAndHighlightBorder: false,
 				editable: false,
-				fields: ['CreationDate'],
+				fields: ['PercentDoneByStoryCount'],
 				showAge: true
-			},
-			rowConfig: {
-				field: 'c_TestingType'
+			}
+			// rowConfig: {
+				// field: 'Owner'
+			// }
+		});
+	},
+	
+	_onSelect: function() {
+		var board = this.down('rallycardboard');
+		board.refresh({
+			storeConfig: {
+				filters: [this._getFilter()]
 			}
 		});
+	},
+		
+	_getFilter: function() {
+		var combo = this.down('rallycombobox');
+		return {
+			property: 'Parent.Parent',
+			operator: '=',
+			value: combo.getValue()
+		};
 	}
+
 });
